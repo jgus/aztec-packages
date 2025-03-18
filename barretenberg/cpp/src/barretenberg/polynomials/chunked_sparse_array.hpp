556422aa22b7035e162caa2c862f74ce3f48869b
Josh Gustafson <j@gustafson.me>
2025-03-18 19:45:36 +0000

    move generator

--- barretenberg/cpp/src/barretenberg/polynomials/chunked_sparse_array.hpp
+++ barretenberg/cpp/src/barretenberg/polynomials/chunked_sparse_array.hpp
@@ -1,65 +1,11 @@
 #pragma once
 
-#include <coroutine>
 #include <cstddef>
+#include <span>
 #include <stdexcept>
 
 #include "barretenberg/common/assert.hpp"
-
-// Since C++20 doesn't have std::generator
-// See https://en.cppreference.com/w/cpp/language/coroutines#co_yield
-template <typename T> struct Generator {
-    struct promise_type;
-    using handle_type = std::coroutine_handle<promise_type>;
-
-    struct promise_type {
-        T value_;
-        std::exception_ptr exception_;
-
-        Generator get_return_object() { return Generator(handle_type::from_promise(*this)); }
-        std::suspend_always initial_suspend() { return {}; }
-        std::suspend_always final_suspend() noexcept { return {}; }
-        void unhandled_exception() { exception_ = std::current_exception(); }
-
-        template <std::convertible_to<T> From> std::suspend_always yield_value(From&& from)
-        {
-            value_ = std::forward<From>(from);
-            return {};
-        }
-        void return_void() {}
-    };
-
-    handle_type h_;
-
-    Generator(handle_type h)
-        : h_(h)
-    {}
-    ~Generator() { h_.destroy(); }
-    explicit operator bool()
-    {
-        fill();
-        return !h_.done();
-    }
-    T operator()()
-    {
-        fill();
-        full_ = false;
-        return std::move(h_.promise().value_);
-    }
-
-  private:
-    bool full_ = false;
-
-    void fill()
-    {
-        if (!full_) {
-            h_();
-            if (h_.promise().exception_)
-                std::rethrow_exception(h_.promise().exception_);
-            full_ = true;
-        }
-    }
-};
+#include "generator.hpp"
 
 template <typename T, T DefaultValue = T{}> class ChunkedSparseArray {
   public:
@@ -133,10 +79,10 @@ template <typename T, T DefaultValue = T{}> class ChunkedSparseArray {
         i->second[offset] = std::forward<U>(value);
     }
 
-    Generator<std::pair<size_t, std::vector<T>*>> chunks()
+    Generator<std::pair<size_t, std::span<T>>> chunks()
     {
         for (auto i = chunks_.begin(); i != chunks_.end(); ++i)
-            co_yield std::pair<size_t, std::vector<T>*>{ i->first, &(i->second) };
+            co_yield std::pair<size_t, std::span<T>>{ i->first, i->second };
     }
 
     Generator<std::pair<size_t, T>> entries()
