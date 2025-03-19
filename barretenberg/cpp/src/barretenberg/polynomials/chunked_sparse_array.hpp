5fc061c986dbc962b53bac266b39cec59df9fbe6
Josh Gustafson <j@gustafson.me>
2025-03-19 20:06:15 +0000

    rename

--- barretenberg/cpp/src/barretenberg/polynomials/chunked_sparse_array.hpp
+++ barretenberg/cpp/src/barretenberg/polynomials/chunked_sparse_array.hpp
@@ -17,10 +17,10 @@ template <typename T, T DefaultValue = T{}> class ChunkedSparseArray {
 
     T get(size_t index) const
     {
-        auto i = find_chunk(index);
-        if (i == chunks_.end())
+        auto i = find_span(index);
+        if (i == spans_.end())
             return DefaultValue;
-        return chunk_element(i, index);
+        return span_element(i, index);
     }
 
     auto allocate(size_t begin, size_t end)
@@ -29,80 +29,80 @@ template <typename T, T DefaultValue = T{}> class ChunkedSparseArray {
             throw std::out_of_range("bad index");
 
         if (end <= begin)
-            return chunks_.end();
+            return spans_.end();
 
-        if (chunks_.empty()) {
-            return chunks_.emplace(end, std::vector<T>(end - begin, DefaultValue)).first;
+        if (spans_.empty()) {
+            return spans_.emplace(end, std::vector<T>(end - begin, DefaultValue)).first;
         }
 
-        auto i = chunks_.lower_bound(begin);
+        auto i = spans_.lower_bound(begin);
 
-        size_t prefix_size = (begin < chunk_begin(i)) ? 0 : begin - chunk_begin(i);
+        size_t prefix_size = (begin < span_begin(i)) ? 0 : begin - span_begin(i);
 
-        std::vector<T> new_chunk;
-        new_chunk.reserve(end - begin + prefix_size);
+        std::vector<T> new_span;
+        new_span.reserve(end - begin + prefix_size);
 
         while (begin < end) {
-            if (begin < chunk_begin(i)) {
-                if (end < chunk_begin(i)) {
-                    new_chunk.insert(new_chunk.end(), end - begin, DefaultValue);
+            if (begin < span_begin(i)) {
+                if (end < span_begin(i)) {
+                    new_span.insert(new_span.end(), end - begin, DefaultValue);
                     break;
                 }
-                new_chunk.insert(new_chunk.end(), chunk_begin(i) - begin, DefaultValue);
-                begin = chunk_begin(i);
+                new_span.insert(new_span.end(), span_begin(i) - begin, DefaultValue);
+                begin = span_begin(i);
             }
-            new_chunk.insert(new_chunk.end(), i->second.begin(), i->second.end());
+            new_span.insert(new_span.end(), i->second.begin(), i->second.end());
             begin = i->first;
             end = std::max(begin, end);
-            i = chunks_.erase(i);
+            i = spans_.erase(i);
         }
 
-        return chunks_.emplace(end, std::move(new_chunk)).first;
+        return spans_.emplace(end, std::move(new_span)).first;
     }
 
     template <typename U> void set(size_t index, U&& value)
     {
         auto i = allocate(index, index + 1);
-        ASSERT(i != chunks_.end());
-        chunk_element(i, index) = std::forward<U>(value);
+        ASSERT(i != spans_.end());
+        span_element(i, index) = std::forward<U>(value);
     }
 
     Generator<std::pair<size_t, std::span<T const>>> spans() const
     {
-        for (auto i = chunks_.begin(); i != chunks_.end(); ++i)
-            co_yield std::pair<size_t, std::span<T const>>{ chunk_begin(i), i->second };
+        for (auto i = spans_.begin(); i != spans_.end(); ++i)
+            co_yield std::pair<size_t, std::span<T const>>{ span_begin(i), i->second };
     }
 
     Generator<std::pair<size_t, T>> entries() const
     {
-        for (auto i = chunks_.begin(); i != chunks_.end(); ++i)
+        for (auto i = spans_.begin(); i != spans_.end(); ++i)
             for (size_t j = 0; j < i->second.size(); ++j)
-                co_yield std::pair<size_t, T>{ chunk_begin(i) + j, i->second[j] };
+                co_yield std::pair<size_t, T>{ span_begin(i) + j, i->second[j] };
     }
 
   private:
     size_t size_;
-    std::map<size_t, std::vector<T>> chunks_;
+    std::map<size_t, std::vector<T>> spans_;
 
-    static size_t chunk_begin(auto const& i) { return i->first - i->second.size(); }
+    static size_t span_begin(auto const& i) { return i->first - i->second.size(); }
 
-    static auto& chunk_element(auto const& i, size_t index) { return i->second[index - chunk_begin(i)]; }
+    static auto& span_element(auto const& i, size_t index) { return i->second[index - span_begin(i)]; }
 
-    auto find_chunk(size_t index) const
+    auto find_span(size_t index) const
     {
         if (size_ <= index)
             throw std::out_of_range("bad index");
 
-        if (chunks_.empty())
-            return chunks_.end();
+        if (spans_.empty())
+            return spans_.end();
 
-        auto i = chunks_.upper_bound(index);
-        if (i == chunks_.end())
+        auto i = spans_.upper_bound(index);
+        if (i == spans_.end())
             return i;
 
         ASSERT(index < i->first);
-        if (index < chunk_begin(i))
-            return chunks_.end();
+        if (index < span_begin(i))
+            return spans_.end();
         return i;
     }
 };
